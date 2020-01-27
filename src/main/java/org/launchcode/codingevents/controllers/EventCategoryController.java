235d821230a7ec8c8883ee2648d1c882cc91403f
mjbanvard <mjbanvard@gmail.com>
2020-01-26 18:44:54 -0600

    Not Quite Running

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -0,0 +1,55 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.EventCategoryRepository;
+import org.launchcode.codingevents.data.EventRepository;
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.Properties;
+
+@Controller
+@RequestMapping("events")
+public class EventCategoryController {
+
+    @Autowired
+    private EventCategoryController eventCategoryController;
+
+    // * Created for process handler
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", eventCategoryRepository.findAll());
+        return "eventsCategories/index";
+    }
+
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Category");
+        model.addAttribute(new Event());
+        return "eventCategories/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            model.addAttribute(new Event());
+            return "eventCategories/create";
+        }
+        return "redirect:";
+    }
+
+}
